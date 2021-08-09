08ce6503fc19ebb4142dcfa4c2e02362ae3b065c
andresportillov <39763652+andresportillov@users.noreply.github.com>
2021-08-09 16:58:31 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ import { Schema, model, version } from "mongoose";
 
 const userSchema = new Schema({
     _id: { type: String, required: true },
-    firstname: { type: String, required: true },
+    firstname: { type: String },
     lastname: { type: String },
-    age: String,
+    age: { type: String },
     email: { type: String, required: true },
     occupation: { type: String },
     password: { type: String, required: true },
